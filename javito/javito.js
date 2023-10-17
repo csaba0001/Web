468abc7c3c3dfe1445622bdbd1814518f8f9d6d6
kbnt <77972833+csaba0001@users.noreply.github.com>
2023-10-17 09:27:52 +0200

    Javító dolgozat

---
+++ javito/javito.js
@@ -0,0 +1,12 @@
+let d1 = document.getElementById("tar1");
+let d2 = document.getElementById("tar2");
+let d3 = document.getElementById("tar3");
+let container = document.getElementById("container");
+let body = document.getElementById("body");
+
+
+d1.style.backgroundColor = "#EB8F90";
+d2.style.backgroundColor = "#FFB471";
+d3.style.backgroundColor = "#ADBED2";
+container.style.backgroundColor = "#12406A";
+body.style.backgroundColor = "#CCFFFF";
